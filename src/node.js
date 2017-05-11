2cc81249999c386d1d27af1d533920b29711c8e2
Giuseppe <bartgb00@gmail.com>
2017-05-11 11:32:46 -0300

    Added horizontal and vertical search

--- src/node.js
+++ src/node.js
@@ -1,4 +1,3 @@
-const cloneDeep = require('clone-deep')
 const _ = require('lodash')
 
 module.exports = class Node {
@@ -12,7 +11,7 @@ module.exports = class Node {
 
   createChild(position, player) {
     for (let i = 0; i < this.state.length; i++) {
-      let newState = cloneDeep(this.state)
+      let newState = _.cloneDeep(this.state)
       if (newState[i][position] !== 0) {
         newState[i - 1][position] = player
         this.children.push(new Node(this, newState, this.alpha, this.beta))
@@ -22,11 +21,20 @@ module.exports = class Node {
   }
 
   findPieces(player) {
+    let pieces = [];
+    this.horizontalSearch(player).forEach((piece) => {
+      pieces.push(piece);
+    });
+    this.verticalSearch(player).forEach((piece) => {
+      pieces.push(piece);
+    });
+    return pieces;
+  }
+
+  horizontalSearch(player) {
     let found = false;
     let pieceSize = 0;
     let pieces = [];
-
-    // Horizontal Search
     for (let i = 0; i < this.state.length; i++) {
       found = false;
       pieceSize = 0;
@@ -45,7 +53,7 @@ module.exports = class Node {
           }
           pieceSize = 0;
         }
-        if (found && j === this.state[i].length - 1) {
+        if (found && j === this.state[i].length - 1 && pieceSize > 1) {
           let temp = [];
           for (let k of _.range(pieceSize)) {
             temp.push([j - k, i]);
@@ -54,8 +62,40 @@ module.exports = class Node {
         }
       }
     }
+    return pieces;
+  }
 
-
+  verticalSearch(player) {
+    let found = false;
+    let pieceSize = 0;
+    let pieces = [];
+    for (let j = 0; j < this.state[0].length; j++) {
+      found = false;
+      pieceSize = 0;
+      for (var i = 0; i < this.state.length; i++) {
+        if (this.state[i][j] === player) {
+          found = true;
+          pieceSize++;
+        } else {
+          found = false;
+          if (pieceSize > 1) {
+            let temp = [];
+            for (let k of _.range(1, pieceSize + 1)) {
+              temp.push([j, i - k]);
+            }
+            pieces.push(temp);
+          }
+          pieceSize = 0;
+        }
+        if (found && i === this.state.length - 1 && pieceSize > 1) {
+          let temp = [];
+          for (let k of _.range(pieceSize)) {
+            temp.push([j, i - k]);
+          }
+          pieces.push(temp);
+        }
+      }
+    }
     return pieces;
   }
 }
