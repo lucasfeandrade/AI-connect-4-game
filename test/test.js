1da5d8eafbfe835d997ca65f6e1fc3110aa28f97
Giuseppe <bartgb00@gmail.com>
2017-05-14 14:22:59 -0300

    Updated tests

--- test/test.js
+++ test/test.js
@@ -26,16 +26,14 @@ describe('Testing Node Class', function() {
     ]
 
     it('should create a child', function() {
-      let newNode = new State(undefined, initialState, 0, 0, 2)
-      newNode.createChild(2, 1)
-      let newChild = newNode.children.shift()
+      let newNode = new State(undefined, initialState, 0, 0, 0)
+      let newChild = newNode.createChild(2, 1)
       assert.equal(typeof newNode, typeof newChild)
     });
 
     it('should put player one at third column', function() {
-      let newNode = new State(undefined, initialState, 0, 0, 2)
-      newNode.createChild(2, 1)
-      let newChild = newNode.children.shift()
+      let newNode = new State(undefined, initialState, 0, 0, 0)
+      let newChild = newNode.createChild(2, 1)
       assert.deepEqual(finalState, newChild.state)
     });
   });
@@ -53,7 +51,6 @@ describe('Testing Node Class', function() {
     it('should find a 3-piece', function() {
       let newNode = new State(undefined, initialState, 0, 0, 2)
       let pieces = newNode.findPieces(2)
-      console.log(pieces);
       let expPieces = [
         [
           [0, 5],
@@ -78,12 +75,13 @@ describe('Testing Node Class', function() {
       [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0],
-      [0, 1, 2, 0, 0, 0, 0],
-      [2, 0, 2, 0, 2, 2, 2]
+      [0, 2, 1, 0, 0, 0, 0],
+      [1, 0, 1, 0, 1, 1, 1]
     ]
 
-    let newNode = new State(undefined, initialState, 0, 0, 2)
+    let newNode = new State(undefined, initialState, 0, 0, 1)
     let value = newNode.findUtility()
+    console.log(value);
     it('return value expected value for one vertical pair and one horizontal triple', function() {
       assert.equal(value, 9240)
     });
@@ -111,7 +109,7 @@ describe('Testing Node Class', function() {
     let secondNode = new State(undefined, secondState, 0, 0, 2)
 
     it('should return true for all columns', function() {
-      for (let position = 0; position < 6; position ++) {
+      for (let position = 0; position < 6; position++) {
         assert.equal(firstNode.isColumnFull(position), true)
       }
     });
@@ -120,44 +118,86 @@ describe('Testing Node Class', function() {
     });
   });
 
-  describe('isLeaf', function() {
+  describe('isBoardFull', function() {
+    const initialState = [
+      [2, 2, 1, 2, 1, 2, 1],
+      [1, 2, 1, 1, 1, 2, 2],
+      [2, 2, 1, 2, 1, 2, 2],
+      [2, 2, 1, 1, 1, 2, 2],
+      [2, 1, 2, 2, 2, 2, 2],
+      [2, 1, 2, 1, 2, 1, 2]
+    ]
+    const secondState = [
+      [2, 2, 0, 2, 1, 2, 1],
+      [1, 2, 1, 1, 1, 2, 2],
+      [2, 2, 1, 2, 1, 2, 2],
+      [2, 2, 1, 1, 1, 2, 2],
+      [2, 1, 2, 2, 2, 2, 2],
+      [2, 1, 2, 1, 2, 1, 2]
+    ]
+
+    let firstNode = new State(undefined, initialState, 0, 0, 2)
+    let secondNode = new State(undefined, secondState, 0, 0, 2)
+
+    it('should return true', function() {
+      assert.equal(firstNode.isBoardFull(), true)
+    });
+    it('should return false', function() {
+      assert.equal(secondNode.isBoardFull(), false)
+    });
+  });
+
+  describe('isGameOver', function() {
+    const startState = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0]
+    ]
     const quadraState = [
-      [0, 0, 0, 0, 0, 0, 1],
-      [0, 0, 0, 0, 0, 2, 2],
-      [2, 0, 0, 2, 1, 0, 0],
-      [2, 0, 0, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
       [2, 0, 0, 0, 0, 0, 0],
-      [2, 0, 0, 0, 0, 0, 0]
+      [2, 0, 0, 0, 0, 0, 0],
+      [2, 0, 0, 2, 1, 0, 1],
+      [2, 0, 0, 1, 1, 2, 2]
     ]
     const fullState = [
-      [1, 2, 1, 2, 1, 2, 1],
-      [2, 1, 2, 1, 2, 2, 2],
-      [1, 2, 2, 2, 0, 0, 0],
-      [2, 1, 2, 1, 1, 0, 0],
-      [1, 0, 1, 2, 0, 0, 0],
-      [2, 1, 2, 1, 0, 0, 0]
+      [1, 2, 2, 1, 1, 2, 1],
+      [2, 1, 1, 1, 2, 2, 2],
+      [2, 1, 2, 2, 1, 2, 1],
+      [2, 1, 1, 2, 1, 1, 2],
+      [1, 2, 1, 2, 2, 1, 2],
+      [2, 2, 2, 1, 2, 1, 1]
     ]
     const notFinishedState = [
-      [0, 0, 0, 0, 0, 0, 1],
-      [0, 0, 0, 0, 0, 2, 2],
-      [0, 0, 0, 2, 1, 0, 0],
-      [2, 0, 0, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
       [2, 0, 0, 0, 0, 0, 0],
-      [2, 0, 0, 0, 0, 0, 0]
+      [2, 0, 0, 2, 1, 0, 1],
+      [2, 0, 0, 1, 1, 2, 2]
     ]
 
-    let firstNode = new State(undefined, quadraState, 0, 0, 2)
-    let notFinishedNode = new State(undefined, notFinishedState, 0, 0, 2)
-    let fullNode = new State(undefined, fullState, 0, 0, 2)
+    let startNode = new State(undefined, startState, 0, 0, 0)
+    let firstNode = new State(undefined, quadraState, 0, 0, 0)
+    let notFinishedNode = new State(undefined, notFinishedState, 0, 0, 0)
+    let fullNode = new State(undefined, fullState, 0, 0, 0)
+
+    it('should return false (empty board)', function() {
+      assert.equal(startNode.isGameOver(), false)
+    });
 
     it('should return true (it has a quadra and is not full)', function() {
-      assert.equal(firstNode.isLeaf(), true)
+      assert.equal(firstNode.isGameOver(), true)
     });
     it('should return false (there is no quadra and is not full)', function() {
-      assert.equal(notFinishedNode.isLeaf(), false)
+      assert.equal(notFinishedNode.isGameOver(), false)
     });
     it('should return true (there is no quadra and but it is full)', function() {
-      assert.equal(fullNode.isLeaf(), true)
+      assert.equal(fullNode.isGameOver(), true)
     });
   });
 
